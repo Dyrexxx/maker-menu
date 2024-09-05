7f6e0ffe8f2d8d826c001feb2292f8371657e3b0
dyrex <Dimut280501@gmail.com>
2024-09-05 14:56:48 +0300

    Настроен скрипт для dev профиля

--- src/main/resources/db/scripts/data/import_ingredient_data.sql
+++ src/main/resources/db/scripts/data/import_ingredient_data.sql
@@ -1,5 +1,7 @@
-insert into ingredient(title, min_weight) values ('Молоко', 300);
-insert into ingredient(title, min_weight) values ('Тесто', 100);
-insert into ingredient(title, min_weight) values ('Супер соус', 400);
-insert into ingredient(title, min_weight) values ('Сахар', 20);
+insert into ingredient(title, min_weight) values ('Тесто', 2000);
+insert into ingredient(title, min_weight) values ('Сыр', 3000);
+insert into ingredient(title, min_weight) values ('Особый сыр', 1000);
+insert into ingredient(title, min_weight) values ('Кетчуп', 1000);
+insert into ingredient(title, min_weight) values ('Особый кетчуп', 500);
+insert into ingredient(title, min_weight) values ('Секретный ингредиент', 1000);
 
